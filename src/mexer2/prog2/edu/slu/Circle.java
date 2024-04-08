9afa8010c637ae3666741ecbdaa9acaa604e0478
Yu <2200277@slu.edu.ph>
2024-04-08 16:57:40 +0800

    create Circle.java documentation; kung sino man author netong class palagay nalang name niyo sa author salamats

--- src/mexer2/prog2/edu/slu/Circle.java
+++ src/mexer2/prog2/edu/slu/Circle.java
@@ -2,15 +2,36 @@ package mexer2.prog2.edu.slu;
 
 /**
  * A reference class for creating a circle.
- * @documentation Oliver
+ * Inherits values and attributes from the Shape class.
+ *
+ * @documentation Oliver Yu
+ * @author
  */
 public class Circle extends Shape {
+
+    /**
+     * Radius of the circle.
+     * The radius is the distance from the center of the circle to any point of its circumference.
+     */
     private final double radius;
+
+    /**
+     * Parameterized constructor of a circle, containing a name and a radius.
+     *
+     * @param name of the shape
+     * @param radius of the circle
+     */
     public Circle(String name, double radius) {
         super(name);
         this.radius = radius;
     }
 
+    /**
+     * Computes the area of the circle by using the formula π(r^2)
+     * where r is the radius of the circle.
+     *
+     * @return area of the circle
+     */
     @Override
     public double area() {
         double area = Math.PI * Math.pow(radius,2);
@@ -18,6 +39,12 @@ public class Circle extends Shape {
         return Double.parseDouble(areaString);
     }
 
+    /**
+     * Computes the perimeter of the circle by using the formula 2πr
+     * where r is the radius of the circle.
+     *
+     * @return perimeter of the circle
+     */
     @Override
     public double perimeter() {
         double perimeter = Math.PI * (2 * radius);
