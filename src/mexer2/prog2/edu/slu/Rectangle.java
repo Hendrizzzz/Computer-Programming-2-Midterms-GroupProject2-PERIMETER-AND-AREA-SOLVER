65f81c8b650518b5a098d6d9d05f64885b1c2559
paulpajara <146008696+paulpajara@users.noreply.github.com>
2024-04-08 23:31:52 +0800

    Update Rectangle.java

--- src/mexer2/prog2/edu/slu/Rectangle.java
+++ src/mexer2/prog2/edu/slu/Rectangle.java
@@ -1,12 +1,36 @@
 package mexer2.prog2.edu.slu;
 
-public class Rectangle extends Square{
+/**
+ * A reference class for creating a rectangle, a quadrilateral with four right angles.
+ * It inherits values and attributes from the Square class.
+ *
+ * @author Paul Pajara
+ */
+public class Rectangle extends Square {
+
+    /**
+     * The length of the second side of the rectangle.
+     */
     private final double side2;
+
+    /**
+     * Constructs a rectangle object with the specified name, side lengths.
+     *
+     * @param name  the name of the rectangle
+     * @param side1 the length of the first side of the rectangle
+     * @param side2 the length of the second side of the rectangle
+     */
     public Rectangle(String name, double side1, double side2) {
         super(name, side1);
         this.side2 = side2;
     }
 
+    /**
+     * Computes and returns the area of the rectangle.
+     * The area of a rectangle is calculated by multiplying the lengths of its two sides.
+     *
+     * @return the area of the rectangle
+     */
     @Override
     public double area() {
         double area = side2 * side;
@@ -14,9 +38,15 @@ public class Rectangle extends Square{
         return Double.parseDouble(areaString);
     }
 
+    /**
+     * Computes and returns the perimeter of the rectangle.
+     * The perimeter of a rectangle is calculated by adding the lengths of all its sides.
+     *
+     * @return the perimeter of the rectangle
+     */
     @Override
     public double perimeter() {
-        double perimeter = 2 * side2 + 2 * side;
+        double perimeter = 2*(side2 + side);
         String perimeterString = String.format("%.5f", perimeter);
         return Double.parseDouble(perimeterString);
     }
